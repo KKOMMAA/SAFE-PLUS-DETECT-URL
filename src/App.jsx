62ba17101559e1b5c3862618f483c386defdc71a
pinktopaz <heather128@ewhain.net>
2023-11-22 02:01:15 +0900

    init : eslint, prettier 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
 import Router from './components/common/Router';
-import GlobalStyle from './styles/GlobalStyle';
-import { ThemeProvider } from 'styled-components';
-import { theme } from './styles/theme';
 import { RecoilRoot } from 'recoil';
 
 function App() {
   return (
-    <ThemeProvider theme={{ ...theme }}>
-      <RecoilRoot>
-        <Router />
-        <GlobalStyle />
-      </RecoilRoot>
-    </ThemeProvider>
+    <RecoilRoot>
+      <Router />
+      hi
+    </RecoilRoot>
   );
 }
 
