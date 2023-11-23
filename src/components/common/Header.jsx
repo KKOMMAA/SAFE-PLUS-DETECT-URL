15b8cb57fa45215125b3101a712f4049012170d5
pinktopaz <heather128@ewhain.net>
2023-11-23 21:21:15 +0900

    ui : 헤더 공통컴포넌트 퍼블리싱

---
+++ src/components/common/Header.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import backIcon from '../../assets/icon/Ic_header_back.svg';
+
+export default function Header({ title }) {
+  return (
+    <HeaderWrapper>
+      <img src={backIcon} alt="뒤로가기" />
+      <h1>{title}</h1>
+    </HeaderWrapper>
+  );
+}
+
+const HeaderWrapper = styled.header`
+  width: 100%;
+  height: 6.8rem;
+
+  position: relative;
+
+  > img {
+    position: absolute;
+    margin-left: 3.6rem;
+    top: 50%;
+    transform: translate(0%, -50%);
+  }
+
+  > h1 {
+    width: 7rem;
+    color: #000;
+    font-size: 1.8rem;
+    font-weight: 600;
+
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+`;
